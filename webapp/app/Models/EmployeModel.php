92e28991f0706970ed094e0cd840fa7f3d7c4036
Nia <ainitokiana@gmail.com>
2026-05-13 09:29:57 +0300

    création des models

---
+++ webapp/app/Models/EmployeModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class EmployeModel extends Model
+{
+    protected $table = 'employes';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $protectFields = true;
+
+    protected $allowedFields = [
+        'nom',
+        'prenom',
+        'email',
+        'password',
+        'role',
+        'departement_id',
+        'date_embauche',
+        'actif',
+    ];
+}
