92e291e8156b7f6e971ff447083f34fd3eea9b08
OvyFrite <rakotozananyandy00@gmail.com>
2026-05-13 09:31:12 +0300

    routage

--- webapp/app/Config/Routes.php
+++ webapp/app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/rh', 'Home::rh');
+
+$routes->get('/employe', 'Home::employe');
+$routes->get('/employe/dashboard', 'Home::employeDashboard');
+$routes->get('/employe/create', 'Home::create');
+
+$routes->get('/admin', 'Home::admin');
+$routes->get('/admin/dashboard', 'Home::adminDashboard');
--- webapp/app/Controllers/Home.php
+++ webapp/app/Controllers/Home.php
@@ -6,6 +6,36 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('auth/login');
+    }
+
+    public function rh(): string
+    {
+        return view('rh/index');
+    }
+
+    public function employe(): string
+    {
+        return view('employe/index');
+    }
+
+    public function employeDashboard(): string
+    {
+        return view('employe/dashboard');
+    }
+
+    public function create(): string
+    {
+        return view('employe/create');
+    }
+
+    public function admin(): string
+    {
+        return view('admin/employes');
+    }
+
+    public function adminDashboard(): string
+    {
+        return view('admin/dashboard');
     }
 }
